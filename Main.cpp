f8c1c54f13c1e620c21d6536c2a1723ddf5c3017
Alex <alexcs2013@mail.ru>
2020-04-08 00:22:32 +0300

    Добавил конструктор копирования, присваивания

--- Main.cpp
+++ Main.cpp
@@ -4,12 +4,15 @@ using namespace std;
 class AString
 {
 public:
+
+	//конструктор класса без входящих параметров
 	AString() 
 	{
-		
+		str = nullptr;
 	}
 
-	AString(char* str)
+	//конструктор класса с входящим параметром типа char
+	AString(const char *str)
 	{
 		int length = strlen(str);
 		this->str = new char[length + 1];
@@ -20,28 +23,65 @@ public:
 		this->str[length] = '\0';
 	}
 
+	//деструктор класса
 	~AString()
 	{
 		delete[] this->str;
 	}
 
+	//оператор присваивания
+	AString& operator=(const AString & other)
+	{
+		if (this->str!=nullptr)
+		{
+			delete[] str;
+		}
+		int length = strlen(other.str);
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		this->str[length] = '\0';
+		return *this;
+	}
+
+	//конструктор копирования
+	AString(const AString & other)
+	{
+		int length = strlen(other.str);
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		this->str[length] = '\0';
+	}
+
+	//конструктор сложения двух строчек
+	AString operator+(const AString & other)
+	{
+
+	}
+
+	//метод вывода строчек в командную строку
+	void print(const AString& other)
+	{
+
+	}
 private:
 	char* str;
 };
 
 
 
-
-
-
-
-
 int main() 
 {
-	AString str;
+	AString str="hello";
 
+	AString str1("hello");
 
-
+	
 	return 0;
 }
 
